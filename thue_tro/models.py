2ac9cf17a7cde40b914bc534de894b5771d1e8b4
Administrator <vutrongtin02072004@gmail.com>
2025-03-21 17:58:01 +0700

    Add base model and rental post structure to models.py

--- thue_tro/models.py
+++ thue_tro/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class BaseModel(models.Model):
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-id']
+
+class Post(BaseModel):
+    title = models.CharField()
+    content = models.TextField(null = True)
+
+class RentalPost(Post):
+    province = models.CharField()
+    city = models.CharField()
+    address = models.CharField()
+    price = models.FloatField()
+    limit_person = models.IntegerChoices()
+    area = models.FloatField()
+    number_of_bedrooms = models.IntegerChoices()
+    number_of_bathrooms = models.IntegerChoices()
+    utilities = models.Choices
+
+    
+
