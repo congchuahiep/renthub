e57000b8798c448a9e26d51e60ee39e334f57db9
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-04-04 16:12:05 +0700

    Refactor serializers.py: Improve image handling and reorganize imports

--- core/serializers.py
+++ core/serializers.py
@@ -1,20 +1,29 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
-from core.models import RentalPost, RoomSeekingPost, Conversation, Message, CommentPost, BoardingHouse
+from core.models import (
+    RentalPost,
+    RoomSeekingPost,
+    Conversation,
+    Message,
+    CommentPost,
+    BoardingHouse,
+)
 
-class ItemSerializer(ModelSerializer):
+
+class ModelWithImageSerializer(ModelSerializer):
+    # this function run before serialization time
     def to_representation(self, instance):
-        return super().to_representation(instance)
-        if instance.image:
-            data['image'] = instance.image.url
+        data = super().to_representation(instance)
+        # change raw image to cloudinary url
+        data["image"] = instance.image.url if instance.image else ""
         return data
-    
 
-class BoardingHouseSerializer(ItemSerializer):
+
+class BoardingHouseSerializer(ModelWithImageSerializer):
     class meta:
         model = BoardingHouse
-        fields = ['id','boarding_house_name','address','image']
+        fields = ["id", "boarding_house_name", "address", "image"]
 
 
-class RentalPostSerializer():
+class RentalPostSerializer:
     class meta:
-        fields = ['']
+        fields = [""]
