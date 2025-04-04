2509f9678d1a0c301b7b393abf18cf18e40caf4e
Administrator <vutrongtin02072004@gmail.com>
2025-04-04 15:52:43 +0700

    merge model

--- core/serializers.py
+++ core/serializers.py
@@ -4,4 +4,17 @@ from core.models import RentalPost, RoomSeekingPost, Conversation, Message, Comm
 class ItemSerializer(ModelSerializer):
     def to_representation(self, instance):
         return super().to_representation(instance)
-  
+        if instance.image:
+            data['image'] = instance.image.url
+        return data
+    
+
+class BoardingHouseSerializer(ItemSerializer):
+    class meta:
+        model = BoardingHouse
+        fields = ['id','boarding_house_name','address','image']
+
+
+class RentalPostSerializer():
+    class meta:
+        fields = ['']
