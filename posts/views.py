c205d54f7efe051ab0b4f28599b5f869e0cb2c21
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-04-13 20:31:20 +0700

    Add support for retrieving single rental posts

--- posts/views.py
+++ posts/views.py
@@ -6,9 +6,9 @@ from posts.serializers import RentalPostSerializer
 
 
 # Create your views here.
-class RentalPostViewSet(viewsets.ViewSet, generics.ListAPIView):
-    # Sử dụng prefetch_related("ultilitiess") để tối ưu hóa truy vấn
-    queryset = RentalPost.objects.prefetch_related("utilities").filter(active=True)
+class RentalPostViewSet(viewsets.ViewSet, generics.ListAPIView, generics.RetrieveAPIView):
+    # Sử dụng prefetch_related("utilities", "images") để tối ưu hóa câu truy vấn
+    queryset = RentalPost.objects.prefetch_related("utilities", "images").filter(active=True)
     serializer_class = RentalPostSerializer
     pagination_class = PostPagination
     page_size = 10
