5be34d1e02a4cbbb26164ba770daeb9cae25437b
Administrator <vutrongtin02072004@gmail.com>
2025-04-15 00:13:43 +0700

    Revert "Update viewset, serializer, and urls.py to handle the new field for chats model"
    This reverts commit f8ae661f5479f715aadb28c407d78d74a988fc6f.

--- chats/urls.py
+++ chats/urls.py
@@ -3,18 +3,10 @@ from chats import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
+router.register('conversations', views.ConversationViewSet, basename='conversation')
 
 
 urlpatterns = [
-    path('Chats/<int:id>/message/', views.MessageViewSet.as_view(
-        {'get': 'get_message', 'post': 'create_message'})),
-
-    path('Chats/<int:id>',views.ConversationViewSet.as_view({
-        'post':'create_conversation'
-    }), name='conversation-create'),
-
-    path('Chats/',views.ConversationViewSet.as_view({
-        'get':'get_conversations'
-    }),name='conversation-list'),
-
+    path('Chats/<int:id>/message/', views.MessageViewSet.as_view({'get': 'get_message', 'post': 'create_message'})),
+    path('', include(router.urls)),
 ]
