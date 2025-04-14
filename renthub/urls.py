8f86f20c0a2b70418d114417425cc0884c0c9cf7
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-04-14 15:02:13 +0700

    Configure JWT authentication for API
    The change adds JWT authentication to the Django REST Framework setup, including
    token endpoints and required dependencies.

--- renthub/urls.py
+++ renthub/urls.py
@@ -20,6 +20,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework.routers import re_path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # Schema cho Swagger API
 schema_view = get_schema_view(
@@ -37,6 +38,11 @@ schema_view = get_schema_view(
 
 # Các API của cả dự án Renthub
 urlpatterns = [
+    # Các API chính của dự án Renthub
+    path('', include('admin_site.urls')),
+    path('', include('chats.urls')),
+    path('', include('posts.urls')),
+
     # Swagger API
     re_path(
         r'^swagger(?P<format>\.json|\.yaml)$',
@@ -53,11 +59,11 @@ urlpatterns = [
         schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc'
     ),
+
     #Debug toolbar
     path('__debug__/', include(debug_toolbar.urls)),
 
-    # Các API chính của dự án Renthub
-    path('', include('admin_site.urls')),
-    path('', include('chats.urls')),
-    path('', include('posts.urls')),
+    # JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
