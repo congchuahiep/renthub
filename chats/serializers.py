af0edb0cf1d0db129e3f0a06b6a1d4ba233aabf4
Administrator <vutrongtin02072004@gmail.com>
2025-04-14 22:55:07 +0700

    Update viewset, serializer, and urls.py to handle the new field for chats model

--- chats/serializers.py
+++ chats/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, Serializer
 from chats.models import Conversation, Message
 from datetime import datetime
@@ -5,7 +6,11 @@ from datetime import datetime
 class ConversationSerializer(ModelSerializer):
     class Meta:
         model = Conversation
-        fields = ['id', 'landlord', 'tenent']
+        fields = '__all__'
+        extra_kwargs = {
+            'landlord': {'required': False},
+            'tenent': {'required': False},
+        }
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
@@ -27,6 +32,34 @@ class ConversationSerializer(ModelSerializer):
         data.pop('tenent') 
 
         return data
+
+    def validate(self, attrs):
+        request = self.context['request']
+        user1 = request.user
+        user2 = self.context.get('user2')
+
+        if user1==user2:
+            raise serializers.ValidationError("Cannot create conversation with yourself.")
+        if user1.user_type == user2.user_type:
+            raise serializers.ValidationError(f"CanNot create conversation with another {user1.user_type}.")
+        
+        if Conversation.objects.filter(
+            landlord=user1,tenent=user2
+        ).exists() or Conversation.objects.filter(
+            landlord=user2, tenent=user1
+        ).exists():
+            raise serializers.ValidationError(f"Conversation between {user1.id} and {user2.id} was exists.")
+        
+        return attrs
+    
+    def create(self, validated_data):
+        user1 = self.context['request'].user
+        user2 = self.context.get('user2')
+        if user1.user_type == 'LR':
+            return Conversation.objects.create(landlord=user1, tenent=user2)
+        else:
+            return Conversation.objects.create(landlord=user2,tenent=user1)
+
 class MessageSerializer(ModelSerializer):
     class Meta:
         model = Message
@@ -41,12 +74,17 @@ class MessageSerializer(ModelSerializer):
         data['create_at'] = datetime.strftime(instance.created_date, "%Y-%m-%d %H:%M:%S")
 
         return data
-
+    
     def validate(self, attrs):
-        sender = attrs.get('sender')  # sender lúc này là instance của User hoặc ID, tùy serializer
-        conversation = attrs.get('conversation')
-
-        
+        sender = attrs.get('sender') 
+        conversation = attrs.get('conversation') 
+        if sender != conversation.landlord and sender != conversation.tenent:
+            raise serializers.ValidationError("Sender must be a participant of the conversation.")
 
         return attrs
+
+    def create(self, validated_data):
+        return Message.objects.create(**validated_data)
+
+
         
