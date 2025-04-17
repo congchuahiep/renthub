5d457184b7a5eba3fb1a28aaf30b85e0e42b098c
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-04-17 14:37:36 +0700

    Fix image URL serialization and improve parameter naming

--- utils/serializers.py
+++ utils/serializers.py
@@ -4,13 +4,13 @@ from .models import Image
 
 class ImageSerializer(serializers.ModelSerializer):
 
-    def to_representation(self, instance):
+    def to_representation(self, image_object):
         """
         Ghi đè phương thức to_representation
         để thêm URL của hình ảnh vào dữ liệu trả về.
         """
-        data = super().to_representation(instance)
-        data['image'] = instance.image.get_url() if instance.image else None
+        data = super().to_representation(image_object)
+        data['image'] = image_object.get_url() if image_object.image else None
         return data
 
     class Meta:
