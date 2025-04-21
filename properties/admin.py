c76f793d3b2c77d350c714bdeed611d649866b42
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-04-21 18:16:35 +0700

    Replace BoardingHouse model with Property model
    The commit refactors the boarding house concept into a more general Property
    model with additional fields for location and approval status.

--- properties/admin.py
+++ properties/admin.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
-
 from admin_site.site import admin_site
-from properties.models import BoardingHouse
+from properties.models import Property
 
 # Register your models here.
-admin_site.register(BoardingHouse)
+admin_site.register(Property)
--- properties/models.py
+++ properties/models.py
@@ -1,20 +1,29 @@
-from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 
 from utils.models import BaseModel
 
 # Create your models here.
-class BoardingHouse(BaseModel):
+class Property(BaseModel):
     """
-    Model này định nghĩa một dãy trọ:
-    Khi chủ trọ mới tạo một tài khoản, bắt buộc chủ trọ phải tạo một dãy trọ,
-    và dãy trọ này sẽ được xét duyệt bởi quản trị viên. Nếu dãy trọ đầu tiên của
-    chủ trọ đã không được xét duyệt, thì tài khoản của chủ trọ sẽ không được phép
-    tạo mới.
+    Model này định nghĩa một dự án bất động sản - aka "Dãy trọ"
 
-    Ngược lại, nếu dãy trọ đã được xét duyệt, thì chủ trọ có thể tạo các bài đăng
+    Khi chủ trọ mới tạo một tài khoản, để tài khoản được kích hoạt, yêu cầu tài khoản chủ
+    trọ phải xác minh một dự án bất động sản (dãy trọ). Nếu dãy trọ đầu tiên của chủ trọ
+    được xét duyệt, tài khoản chủ trọ sẽ được kích hoạt.
+
+    Ngược lại, nếu dãy trọ đầu tiên không được duyệt, thì tài khoản chủ trọ sẽ không được
+    chấp nhận và bị xoá.
     """
 
-    boarding_house_name = models.CharField(max_length=256)
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        APPROVED = 'approved', 'Approved'
+        REJECTED = 'rejected', 'Rejected'
+
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
+
+    properties_name = models.CharField(max_length=256)
+
+    province = models.CharField(max_length=256)
+    city = models.CharField(max_length=256)
     address = models.CharField(max_length=256)
-    images = GenericRelation("utils.Image", null=True)
