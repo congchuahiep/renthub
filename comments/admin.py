056a27772c23681872fc8d10b6cebb4389700b3b
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-04-30 15:01:39 +0700

    Reimplement Comment models in post

--- comments/admin.py
+++ comments/admin.py
@@ -1,5 +1,5 @@
-from admin_site.site import renthub_admin_site
-from comments.models import Comment
+# from admin_site.site import renthub_admin_site
+# from comments.models import Comment
 
-# Register your models here.
-renthub_admin_site.register(Comment)
+# # Register your models here.
+# renthub_admin_site.register(Comment)
