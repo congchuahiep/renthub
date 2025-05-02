7a3516ef9b0f2647f3bd1c53e93621d7ea207d9b
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-05-02 18:27:00 +0700

    Intergrated Google Map
    - Add cordinate to Property
    - Add geocoding.py
    - Add Google map view in Property Admin page

--- properties/admin.py
+++ properties/admin.py
@@ -1,19 +1,22 @@
 from django.utils.html import format_html
 from unfold.admin import ModelAdmin
 
+from admin_site.components import option_display
 from admin_site.site import renthub_admin_site
 from properties.models import Property
+from renthub import settings
+from utils.choices import PropertyStatus
 
 
 # Register your models here.
 class PropertyAdmin(ModelAdmin):
-    list_display = ['name', 'active', 'status']
-    readonly_fields = ['image_gallery']
+    list_display = ['name', 'status_display']
+    readonly_fields = ['image_gallery', "map_view", "latitude", "longitude"]
     
     fieldsets = [
         ("Status", {"fields": ["status"]}),
-        ("Rental post", {"fields": ["name", "owner"]}),
-        ("Location", {"fields": ["province", "district", "address"]}),
+        ("Detail", {"fields": ["name", "owner"]}),
+        ("Location", {"fields": ["address", "latitude", "longitude", "map_view"]}),
         ("Images", {"fields": ["image_gallery"]}),
     ]
     
@@ -30,6 +33,58 @@ class PropertyAdmin(ModelAdmin):
         return format_html(html)
 
     image_gallery.short_description = 'Image Gallery'
+    
+    def status_display(self, property: Property):
+        """Hiển thị loại tài khoản dưới dạng biểu tượng màu."""
+
+        if property.status == PropertyStatus.PENDING:
+            return option_display("Pending", color="yellow")
+        
+        if property.status == PropertyStatus.APPROVED:
+            return option_display("Approved", color="green")
+        
+        if property.status == PropertyStatus.REJECTED:
+            return option_display("Rejected", color="red")
+
+    status_display.short_description = "Status"
+    
+    def map_view(self, property: Property):
+        if not property.latitude or not property.longitude:
+            return "Chưa có tọa độ"
+        html = """
+        <div id="map" style="height: 400px; width: 100%;"></div>
+        <script>
+        
+            function initMap() {{
+                var propertyLocation = {{ lat: {latitude}, lng: {longitude} }};
+                var map = new google.maps.Map(document.getElementById('map'), {{
+                    zoom: 15,
+                    center: propertyLocation,
+                    disableDefaultUI: true,
+                }});
+                var marker = new google.maps.Marker({{
+                    position: propertyLocation,
+                    map: map,
+                    draggable: true,
+                    title: "{property_name}"
+                }});
+                google.maps.event.addListener(marker, 'dragend', function(event) {{
+                    document.getElementById('id_latitude').value = event.latLng.lat();
+                    document.getElementById('id_longitude').value = event.latLng.lng();
+                }});
+            }}
+        </script>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key={api_key}&callback=initMap"></script>
+        """
+        return format_html(
+            html,
+            latitude=property.latitude,
+            longitude=property.longitude,
+            property_name=property.name.replace('"', '\\"'),  # Escape dấu ngoặc kép trong tên
+            api_key=settings.GOOGLE_MAPS_API_KEY    
+        )
+
+    map_view.short_description = "Map"
 
 
 renthub_admin_site.register(Property, PropertyAdmin)
