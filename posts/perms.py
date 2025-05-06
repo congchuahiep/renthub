6bf48afcb0af416a8cae3969d1dc55133501326b
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-05-06 22:02:49 +0700

    Refactor RentalPostViewSet to use mixins for better structure; update permissions and add success message on post creation.

--- posts/perms.py
+++ posts/perms.py
@@ -5,6 +5,7 @@ class IsRentalPostOwner(IsAuthenticated):
     """
     Chỉ cho phép chủ sở hữu bài đăng (cho thuê phòng) truy cập.
     """
+    message = "You are not the owner of this post!"
 
     def has_object_permission(self, request, rental_post_view, rental_post_obj):
         is_authenticated = super().has_permission(request, rental_post_view)
