6d05853dcb7a2ec404073dc0326d4e29b619350e
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-05-08 15:02:41 +0700

    Add CommentViewSet and permissions for comment ownership; update imports in views and urls

--- posts/perms.py
+++ posts/perms.py
@@ -11,3 +11,15 @@ class IsRentalPostOwner(IsAuthenticated):
         is_authenticated = super().has_permission(request, rental_post_view)
         # Kiểm tra nếu người dùng hiện tại là chủ sở hữu bài đăng
         return is_authenticated and rental_post_obj.landlord == request.user
+
+
+class IsCommentOwner(IsAuthenticated):
+    """
+    Chỉ cho phép chủ sở hữu bình luận truy cập.
+    """
+    message = "You are not the owner of this comment!"
+
+    def has_object_permission(self, request, comment_view, comment_obj):
+        is_authenticated = super().has_permission(request, comment_view)
+
+        return is_authenticated and comment_obj.user == request.user
