5353e5b562b562e1153508cb53bb21ea18b69743
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-05-10 18:55:50 +0700

    Enhance RentalPost admin and serializer; add property details and map view in admin, update RentalPostViewSet to filter by approved status, and improve Carousel and Post components in React Native.

--- react_native/components/Post.js
+++ react_native/components/Post.js
@@ -7,15 +7,15 @@ import { useTheme } from 'react-native-paper';
 
 const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
 
-const Post = ({ title, content, images, price }) => {
+const Post = ({ title, content, images, price, address }) => {
 	const theme = useTheme();
 
 	return (
-		<Card>
+		<Card style={{marginBottom: 10}}>
 			<Card.Content style={{marginBottom: 10}}>
 				{images && <Carousel images={images} />}
 				<Text style={[typography.title, { color: theme.colors.primary }]}>{title}</Text>
-				<Text variant="bodyMedium" style={{ color: theme.colors.secondary }}>{content}</Text>
+				<Text variant="bodyMedium" style={{ color: theme.colors.secondary }}>{address}</Text>
 			</Card.Content>
 		</Card>
 	)
